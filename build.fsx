98ef800f2122d600725b3bb231c5a59ffb23c9c1
Nikita Zuev <nikitazu@gmail.com>
2016-02-06 19:10:53 +0300

    fake build run test app

--- build.fsx
+++ build.fsx
@@ -1,11 +1,23 @@
 #r "packages/FAKE/tools/FakeLib.dll"
 open Fake
 
+// Targets
+//
 Target "test" (fun _ ->
     trace "Testing stuff..."
 )
 
 Target "test-app" (fun _ ->
+    let result =
+        ExecProcess (fun info ->
+            info.FileName <- "TestApp.Launcher/bin/Debug/TestApp.Launcher.exe"
+            info.WorkingDirectory <- "TestApp.Launcher/bin/Debug/"
+            info.Arguments <- ""
+        )(System.TimeSpan.FromSeconds 15.)
+    if result <> 0 then failwith ("Test app failed")
+)
+
+Target "build-test-app" (fun _ ->
     !! "TestApp.Library/*.csproj"
     ++ "TestApp.Launcher/*.csproj"
     |> MSBuildDebug "TestApp.Launcher/bin/Debug/" "Build"
@@ -18,7 +30,12 @@ Target "build" (fun _ ->
     |> Log "build-out: "
 )
 
+// Dependencies
+//
 "test"
-   ==> "build"
+    ==> "build"
+
+"build-test-app"
+    ==> "test-app"
 
 RunTargetOrDefault "build"
